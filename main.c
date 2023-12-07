fb1319b10997a44f4430a7603d5255dcfc0159c8
zezomo <zezouniverse7@gmail.com>
2023-12-07 22:32:23 +0000

    zezo2

--- main.c
+++ main.c
@@ -6,55 +6,64 @@
 
 #define MAX_COMMAND_LENGTH 100
 
-void display_prompt() {
-    write(STDOUT_FILENO, "($)\n", 4);
+void display_prompt(void)
+{
+    write(STDOUT_FILENO, "$ /n", 2);
 }
 
-void execute_command(const char *command) {
-    pid_t pid = fork();
+void execute_command(const char *command)
+{
+	pid_t pid; 
+	pid = fork();
 
-    if (pid == -1) {
-        perror("fork");
-    } else if (pid == 0) {
-        execlp(command, command, NULL);
-        perror("execlp");
-        exit(EXIT_FAILURE);
-    } else {
-        waitpid(pid, NULL, 0);
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+	}
+	else if (pid == 0)
+	{
+		execlp(command, command, NULL);
+		perror("execlp");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
 }
 
-int main(int argc, char **argv) {
-    char *input = NULL;
-    size_t len = 0;
-    ssize_t read;
+int main(int argc, char **argv)
+{
+	char *input = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-    if (argc == 1 && isatty(STDIN_FILENO)) {
-        // Interactive mode
-        while (1) {
-            display_prompt();
+	if (argc == 1 && isatty(STDIN_FILENO))
+	{
+		while (1) {
+			display_prompt();
+			read = getline(&input, &len, stdin);
 
-            read = getline(&input, &len, stdin);
+			if (read == -1)
+			{
+				write(STDOUT_FILENO, "\n", 1);
+				break;
+			}
 
-            if (read == -1) {
-                write(STDOUT_FILENO, "\n", 1);
-                break;
-            }
+			input[strcspn(input, "\n")] = 0;
 
-            input[strcspn(input, "\n")] = 0;
-
-            execute_command(input);
-        }
-    } else {
-        // Non-interactive mode
-        while ((read = getline(&input, &len, stdin)) != -1) {
-            input[strcspn(input, "\n")] = 0;
-            execute_command(input);
-        }
-    }
-
-    free(input);
-
-    return 0;
+			execute_command(input);
+		}
+	}
+	else
+	{
+		while ((read = getline(&input, &len, stdin)) != -1)
+		{
+			input[strcspn(input, "\n")] = 0;
+			execute_command(input);
+		}
+	}
+	free(input);
+	return (0);
 }
 
