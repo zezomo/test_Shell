c8b570862ffbccc3a195471e205d1494f5027fec
zezomo <zezouniverse7@gmail.com>
2023-12-08 11:39:42 +0000

    zezo

--- main.c
+++ main.c
@@ -5,65 +5,90 @@
 #include <sys/wait.h>
 
 #define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
 
-void display_prompt(void)
-{
-    write(STDOUT_FILENO, "$ /n", 2);
+void display_prompt() {
+    write(STDOUT_FILENO, "($)\n", 4);
 }
 
-void execute_command(const char *command)
-{
-	pid_t pid; 
-	pid = fork();
+void execute_command(char *command, char **arguments) {
+    pid_t pid = fork();
 
-	if (pid == -1)
-	{
-		perror("fork");
-	}
-	else if (pid == 0)
-	{
-		execlp(command, command, NULL);
-		perror("execlp");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		waitpid(pid, NULL, 0);
-	}
+    if (pid == -1) {
+        perror("fork");
+    } else if (pid == 0) {
+        execvp(command, arguments);
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    } else {
+        waitpid(pid, NULL, 0);
+    }
 }
 
-int main(int argc, char **argv)
-{
-	char *input = NULL;
-	size_t len = 0;
-	ssize_t read;
+int main(int argc, char **argv) {
+    char *input = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-	if (argc == 1 && isatty(STDIN_FILENO))
-	{
-		while (1) {
-			display_prompt();
-			read = getline(&input, &len, stdin);
+    if (argc == 1 && isatty(STDIN_FILENO)) {
+        // Interactive mode
+        while (1) {
+            display_prompt();
 
-			if (read == -1)
-			{
-				write(STDOUT_FILENO, "\n", 1);
-				break;
-			}
+            read = getline(&input, &len, stdin);
 
-			input[strcspn(input, "\n")] = 0;
+            if (read == -1) {
+                write(STDOUT_FILENO, "\n", 1);
+                break;
+            }
 
-			execute_command(input);
-		}
-	}
-	else
-	{
-		while ((read = getline(&input, &len, stdin)) != -1)
-		{
-			input[strcspn(input, "\n")] = 0;
-			execute_command(input);
-		}
-	}
-	free(input);
-	return (0);
+            input[strcspn(input, "\n")] = 0;
+
+            // Tokenize the input into command and arguments
+            char *token;
+            char *tokens[MAX_ARGUMENTS + 2];  // +2 to account for command and NULL terminator
+            int token_count = 0;
+
+            token = strtok(input, " ");
+            while (token != NULL && token_count < MAX_ARGUMENTS + 1) {
+                tokens[token_count++] = token;
+                token = strtok(NULL, " ");
+            }
+
+            tokens[token_count] = NULL;  // NULL terminate the array of arguments
+
+            if (token_count > 0) {
+                // Execute the command with arguments
+                execute_command(tokens[0], tokens);
+            }
+        }
+    } else {
+        // Non-interactive mode
+        while ((read = getline(&input, &len, stdin)) != -1) {
+            input[strcspn(input, "\n")] = 0;
+
+            // Tokenize the input into command and arguments
+            char *token;
+            char *tokens[MAX_ARGUMENTS + 2];
+            int token_count = 0;
+
+            token = strtok(input, " ");
+            while (token != NULL && token_count < MAX_ARGUMENTS + 1) {
+                tokens[token_count++] = token;
+                token = strtok(NULL, " ");
+            }
+
+            tokens[token_count] = NULL;
+
+            if (token_count > 0) {
+                // Execute the command with arguments
+                execute_command(tokens[0], tokens);
+            }
+        }
+    }
+
+    free(input);
+
+    return 0;
 }
 
