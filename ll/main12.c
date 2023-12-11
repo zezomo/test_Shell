999b5b8a00aefd3f12d610ea1328d093e9c378b9
zezomo <zezouniverse7@gmail.com>
2023-12-11 19:39:54 +0200

    final

--- ll/main12.c
+++ ll/main12.c
@@ -6,7 +6,6 @@
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGUMENTS 10
-
 extern char **environ;
 
 void display_prompt()
@@ -18,51 +17,43 @@ void execute_command(char *command, char **arguments)
 {
 	    pid_t pid;
 	        int status;
-		    char **env;
-		        int exit_status;
 
-			    pid = fork();
+		    pid = fork();
 
-			        if (pid == -1)
-					    {
-						            perror("fork");
-							        }
-				    else if (pid == 0)
-					        {
-							        if (strcmp(command, "exit") == 0)
+		        if (pid == -1)
+				    {
+					            perror("fork");
+						        }
+			    else if (pid == 0)
+				        {
+						        if (strcmp(command, "exit") == 0)
+								        {
+										            exit(EXIT_SUCCESS);
+											            }
+							        else
 									        {
-											            exit(EXIT_SUCCESS);
-												            }
-								        else if (strcmp(command, "env") == 0)
-										        {
-												            for (env = environ; *env != NULL; env++)
-														                {
-																	                write(STDOUT_FILENO, *env, strlen(*env));
-																			                write(STDOUT_FILENO, "\n", 1);
-																					            }
-													                exit(EXIT_SUCCESS);
-															        }
-									        else
-											        {
-													            execvp(command, arguments);
-														                perror("execvp");
-																            exit(EXIT_FAILURE);
-																	            }
-										    }
-				        else
-						    {
-							            waitpid(pid, &status, 0);
-
-								            if (WIFEXITED(status))
-										            {
-												                exit_status = WEXITSTATUS(status);
-														            if (exit_status != 0)
-																                {
-																			                exit(exit_status);
-																					            }
+											            execvp(command, arguments);
+												                perror("execvp");
+														            exit(EXIT_FAILURE);
 															            }
-									        }
+								    }
+			        else
+					    {
+						            waitpid(pid, &status, 0);
+
+							            if (WIFEXITED(status))
+									            {
+											                int exit_status = WEXITSTATUS(status);
+													            if (exit_status != 0)
+															                {
+																		                exit(exit_status);
+																				            }
+														            }
+								        }
 }
+
+
+
 
 int find_command(char *command, char *path)
 {
@@ -94,93 +85,101 @@ int find_command(char *command, char *path)
 					    return (found);
 }
 
-void handle_exit_command(char **tokens)
+void handle_env_command(char **tokens)
 {
-	    int exit_status;
+	    char **envp = environ;
 
-	        if (strcmp(tokens[0], "exit") == 0)
-			    {
-				            if (tokens[1] != NULL)
-						            {
-								                exit_status = atoi(tokens[1]);
-										            exit(exit_status);
-											            }
-					            else
-							            {
-									                exit(0);
-											        }
-						        }
+
+	      if (tokens[1] != NULL)
+		        {
+				    fprintf(stderr, "env: unexpected arguments\n");
+				        return;
+					  }
+
+
+	        while (*envp != NULL)
+			  {
+				      printf("%s\n", *envp);
+				          envp++;
+					    }
 }
 
-void handle_env_command(char **tokens) {
-	    char **env;
-	        char *filtered_vars[] = {"GLIBCPP_FORCE_NEW", "GLIBCXX_FORCE_NEW", "LD_PRELOAD", "LD_LIBRARY_PATH", "_", "PWD", NULL};
-		    int i;
 
-		        if (strcmp(tokens[0], "env") == 0) {
+void handle_exit_command(char **tokens)
+{
+	     int exit_status;
 
-				        for (env = environ; *env != NULL; env++) {
-						            for (i = 0; filtered_vars[i] != NULL; i++) {
-								                    if (strncmp(*env, filtered_vars[i], strlen(filtered_vars[i])) == 0) {
-											                        unsetenv(filtered_vars[i]);
-														                    break;
-																                    }
-										                }
-							            }
-
-					        setenv("HBTN", "Holberton", 1);
-
-						        for (env = environ; *env != NULL; env++) {
-								            write(STDOUT_FILENO, *env, strlen(*env));
-									                write(STDOUT_FILENO, "\n", 1);
-											        }
-							    }
+	         if (strcmp(tokens[0], "exit") == 0)
+			     {
+				             if (tokens[1] != NULL)
+						             {
+								                 exit_status = atoi(tokens[1]);
+										             exit(exit_status);
+											             }
+					             else
+							             {
+									                 exit(0);
+											         }
+						         }
 }
 
 void handle_non_exit_command(char **tokens)
 {
-	    char *command = tokens[0];
-	        char *path;
+	  char *command = tokens[0];
+	    char *path;
 
-		    if (access(command, X_OK) != 0)
-			        {
-					        path = getenv("PATH");
-						        if (path != NULL && find_command(command, path))
-								        {
-										            tokens[0] = command;
-											            }
-							        else
-									        {
-											            fprintf(stderr, "%s: command not found\n", command);
-												                return;
-														        }
-								    }
-
-		        execute_command(tokens[0], tokens);
+	      if (access(command, X_OK) != 0)
+		        {
+				    path = getenv("PATH");
+				        if (path != NULL && find_command(command, path))
+						    {
+							          tokens[0] = command;
+								      }
+					    else if
+						          (strcmp(command, "env") == 0)
+								      {
+									            handle_env_command(tokens);
+										          return;
+											      }
+					        else
+							    {
+								          fprintf(stderr, "%s: command not found\n", command);
+									        return;
+										    }
+						  }
+	        execute_command(tokens[0], tokens);
 }
+
 
 void process_input(char *input)
 {
-	    char *token;
-	        char *tokens[MAX_ARGUMENTS + 2];
-		    int token_count = 0;
+	  char *token;
+	    char *tokens[MAX_ARGUMENTS + 2];
+	      int token_count = 0;
 
-		        token = strtok(input, " ");
-			    while (token != NULL && token_count < MAX_ARGUMENTS + 1)
-				        {
-						        tokens[token_count++] = token;
-							        token = strtok(NULL, " ");
-								    }
-
-			        tokens[token_count] = NULL;
-
-				    if (token_count > 0)
-					        {
-							        handle_exit_command(tokens);
-								        handle_env_command(tokens);
+	        token = strtok(input, " ");
+		  while (token != NULL && token_count < MAX_ARGUMENTS + 1)
+			    {
+				        tokens[token_count++] = token;
+					    token = strtok(NULL, " ");
+					      }
+		    tokens[token_count] = NULL;
+		      if (token_count > 0)
+			        {
+					    if (strcmp(tokens[0], "env") == 0)
+						        {
+								      handle_env_command(tokens);
+								          }
+					        else
+							    {
+								          handle_exit_command(tokens);
 									        handle_non_exit_command(tokens);
 										    }
+						  }
 }
+
+
+
 
 int main(int argc, char **argv)
 {
